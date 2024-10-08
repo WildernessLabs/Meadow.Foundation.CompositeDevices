5b7cf1e4a1c445c80c74b92720d515ca3840b746
Chris Tacke <ctacke@opennetcf.com>
2024-10-08 13:20:45 -0500

    Addressing Adrian's academic whining LOL

--- Source/Switches.ChromaTek/Driver/IChromaTekButton.cs
+++ Source/Switches.ChromaTek/Driver/IChromaTekButton.cs
@@ -1,10 +1,11 @@
 ﻿using Meadow.Hardware;
+using Meadow.Peripherals.Sensors;
 
 namespace Meadow.Foundation.Switches.ChromaTek;
 
 /// <summary>
 /// Represents a ChromaTek button with a WS2812 color LED
 /// </summary>
-public interface IChromaTekButton : IColorable
+public interface IChromaTekButton : IColorable, ISensor<bool>
 {
 }
