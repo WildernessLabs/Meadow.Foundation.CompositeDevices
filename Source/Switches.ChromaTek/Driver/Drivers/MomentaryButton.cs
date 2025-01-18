bf2a702d5421b2dbcbdb44f91eee010f23d6fae4
Adrian Stevens <adrian.stevens@gmail.com>
2025-01-18 14:39:58 -0800

    Release 2.0.1.2

---
+++ Source/Switches.ChromaTek/Driver/Drivers/MomentaryButton.cs
@@ -0,0 +1,82 @@
+﻿using Meadow.Foundation.Leds;
+using Meadow.Foundation.Sensors.Buttons;
+using Meadow.Hardware;
+using System;
+
+namespace Meadow.Foundation.Switches.ChromaTek;
+
+/// <summary>
+/// Represents a momentary push button with a WS2812 color LED
+/// </summary>
+public class MomentaryButton : PushButton, IChromaTekButton
+{
+    private ISpiBus? bus = null;
+    private Color color = Color.Black;
+
+    internal Ws2812 LedController { get; set; } = default!;
+    internal int ButtonIndex { get; set; } = 0;
+
+    /// <summary>
+    /// Creates an instance of a ChromaTekMomentaryButton
+    /// </summary>
+    /// <param name="bus">The SPI bus COPI line connected to the WS2812 data line</param>
+    /// <param name="inputPort">The interrupt port connected to the switch</param>
+    public MomentaryButton(IDigitalInterruptPort inputPort, ISpiBus? bus = null)
+        : base(inputPort)
+    {
+        this.bus = bus;
+    }
+
+    /// <summary>
+    /// Creates an instance of a ChromaTekMomentaryButton
+    /// </summary>
+    /// <param name="bus">The SPI bus COPI line connected to the WS2812 data line</param>
+    /// <param name="pin">The IPin connected to the switch</param>
+    /// <param name="resistorMode">The resistor mode for the switch pin</param>
+    public MomentaryButton(IPin pin, ResistorMode resistorMode, ISpiBus? bus = null)
+        : base(pin, resistorMode)
+    {
+        this.bus = bus;
+    }
+
+    /// <summary>
+    /// Creates an instance of a ChromaTekMomentaryButton
+    /// </summary>
+    /// <param name="bus">The SPI bus COPI line connected to the WS2812 data line</param>
+    /// <param name="pin">The IPin connected to the switch</param>
+    /// <param name="resistorMode">The resistor mode for the switch pin</param>
+    /// <param name="debounceDuration">Debounce duration for the interrupt pin</param>
+    public MomentaryButton(IPin pin, ResistorMode resistorMode, TimeSpan debounceDuration, ISpiBus? bus = null)
+        : base(pin, resistorMode, debounceDuration)
+    {
+        this.bus = bus;
+    }
+
+    private void Initialize()
+    {
+        if (LedController != null) return;
+
+        if (bus == null)
+        {
+            throw new Exception("This button must either be constructed withan ISpiBus or added to a ButtonCollection");
+        }
+
+        LedController = new Ws2812(bus, 1);
+    }
+
+    /// <inheritdoc/>
+    public void SetColor(Color color)
+    {
+        if (LedController == null)
+        {
+            Initialize();
+        }
+
+        LedController?.SetLed(ButtonIndex, color);
+        LedController?.Show();
+        this.color = color;
+    }
+
+    /// <inheritdoc/>
+    public Color GetColor() => color;
+}
---
+++ Source/Switches.ChromaTek/Samples/ChromaTek_Sample/wiring.md
@@ -0,0 +1,25 @@
+﻿# ChromaTek Button Wiring
+
+This is the general wiring for a normally open, falling-edge interrupt.  On the left are the button connectors, on the right are the Meadow signals.
+Note that the input port must be pulled high (either internally or with a 10k resistor).
+
+
+**IMPORTANT: The wires for this button use non-typically coloring!  Be careful wiring it!**
+
+```
+BUTTON               MEADOW
+===========================================
+[WHT] COM \  
+           |-------- `GND`  
+[YEL] GND /
+
+[GRN] NO ----------- IDigitalInterruptPort (e.g. `D04`) (pulled up)
+
+[BLU] NC ----------- (not conncted)
+
+[BLK] VDD ---------- `5V`
+
+[RED] DIN ---------- `COPI`
+
+[ORG] DOUT --------- (optional) Next button DIN
+```
